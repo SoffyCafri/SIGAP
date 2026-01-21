ea66af6e10f12ba9009b931dfdd4c3821e1456e2
Soffy <eymi.casas1336@alumnos.udg.mx>
2026-01-21 11:15:14 -0600

    Agrega paquete jazzmin

--- SIGAP/settings.py
+++ SIGAP/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = ['*']
 # APPLICATIONS
 # ==============================
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
